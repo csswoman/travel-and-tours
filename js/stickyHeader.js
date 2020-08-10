39c52085b3eb9b7d1ed473bb7ccd7d8596d9b3a2
Karla Agraz <karla.agraz@gmail.com>
2020-08-10 16:04:32 -0500

    sticky header

---
+++ js/stickyHeader.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+            var fixedHeader = $('#fixedHeader');
+
+            $(window).on('scroll', function () {
+
+                    var proyectosOffsetTop = $('#viajes').offset().top;
+
+                    if ($(window).scrollTop() >= proyectosOffsetTop {
+
+                            fixedHeader.css('margin-top', 0);
+
+                        } else if ($(window).scrollTop() <= proyectosOffsetTop / 2) {
+
+                            fixedHeader.css('margin-top', '-68px')
+                        }
+
+
+                    })
+            });
