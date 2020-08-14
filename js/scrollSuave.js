a02d1c21d21254586071ae84b6f6ac76c12011a8
Karla Agraz <karla.agraz@gmail.com>
2020-08-13 20:49:47 -0500

    aplicando el efecto scroll suave  al menú inicio y volver arriba

--- js/scrollSuave.js
+++ js/scrollSuave.js
@@ -1,4 +1,30 @@
 $(document).ready(function () {
 
     //Mostrar y ocultar botón "volver arriba"
+
+    let btnVolverArriba = $('#btnVolverArriba');
+
+    $(window).on('scroll', function () {
+
+        var proyectosOffsetTop = $('#viajes').offset().top;
+
+        if ($(window).scrollTop() >= proyectosOffsetTop) {
+
+            btnVolverArriba.css('margin-right', 0);
+        } else if ($(window).scrollTop() <= proyectosOffsetTop/2) {
+
+            btnVolverArriba.css('margin-right', '-60px');
+        }
+    })
+
+    // Movimiento suave de scroll de "inicio" y "volver arriba"
+
+    $('a.volver-arriba').on('click', function (e) {
+        e.preventDefault();
+
+        if ($(window).scrollTop() !=0) {
+            $('html, body').animate({scrollTop: 0}, 1000);
+        }
+
+    })
 });
