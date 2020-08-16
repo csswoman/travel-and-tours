bc39e5c853056ffddeef4c8e158f01c1e059d26c
Karla Agraz <karla.agraz@gmail.com>
2020-08-16 07:39:25 -0500

    corección de errores del slider

--- js/slider.js
+++ js/slider.js
@@ -10,23 +10,39 @@ $(document).ready(function(){
 
 
     function moverDerecha() {
-        slider.animate({
-            marginLeft: '-105.6%'
-        }, 700, function(){
-            $('#slider .slide:first').insertAfter('#slider .slide:last');
-            slider.css('margin-left', '-43%');
-        });
+        if(!slider.is(':animated')){
+            slider.animate({
+                marginLeft: '-105.6%'
+            }, 700, function(){
+                $('#slider .slide:first').insertAfter('#slider .slide:last');
+                slider.css('margin-left', '-43%');
+                resetInterval()
+            });
+        }
+
     }
 
     btnSiguiente.on('click', moverDerecha);
 
     function moverIzquierda() {
-        $('#slider .slide:last').insertBefore('#slider .slide:first');
-        slider.css('margin-left', '-105.6%');
-        slider.animate({
+        if(!slider.is(':animated')) {
+            $('#slider .slide:last').insertBefore('#slider .slide:first');
+            slider.css('margin-left', '-105.6%');
+            slider.animate({
             marginLeft: '-43%'
-        }, 700);
+            }, 700, function(){
+                resetInterval()
+            });
+        }
     }
 
     btnAnterior.on('click', moverIzquierda);
+
+
+    var intervalo = setInterval(moverDerecha, 5000);
+
+    function resetInterval() {
+        clearInterval(intervalo);
+        intervalo = setInterval(moverDerecha, 5000);
+    }
 });
