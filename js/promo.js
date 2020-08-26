0d8690799b9d868a9497cc6db32bea1623f775c1
Karla Agraz <karla.agraz@gmail.com>
2020-08-26 03:00:06 -0500

    sección promociones responsive

--- js/promo.js
+++ js/promo.js
@@ -4,6 +4,23 @@ $(document).ready(function(){
         loop: true,
         autoplay: true,
         autoplayTimeout: 2000,
-        autoplayHoverPause: true
+        autoplayHoverPause: true,
+        responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:false
+        }
+    }
+
     });
 })
