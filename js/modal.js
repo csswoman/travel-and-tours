66e764b7a22c191e75fe4e705240ae35a9cc05d1
Karla Agraz <karla.agraz@gmail.com>
2020-08-25 21:56:26 -0500

    efecto lightbox

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,21 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $('.galeria').on('click',function() {
+    $('.imagen-atraccion').on('click', function () {
 
         var rutaImagen = $(this).attr('src');
+        var modal = '<div class="modal" id="modal"><img src="' + rutaImagen + '" alt=""><div class="btn-cerrar" id="btnCerrar"><i class="fa fa-times" aria-hidden="true"></i></div></div>';
+
+        $('#atracciones').after(modal);
+
+        $('#btnCerrar').on('click', function () {
+            $('#modal').remove();
+        })
     })
+
+    $(document).on('keyup', function (e) {
+        if (e.which == 27) {
+            $('#modal').remove();
+        }
+    })
+
 });
